25844a225945127ce481467d7096cc0b3dae82fc
Roshal <roshalsebastian10@gmail.com>
2025-07-28 12:14:46 -0500

    inserted title and plus sign

---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Roshal Sebastian on 7/28/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
